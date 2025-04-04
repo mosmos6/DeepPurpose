81ee6ed679e6411d1ed1490015f8a7eaf15de8c0
mosmos6 <93764745+mosmos6@users.noreply.github.com>
2025-04-04 19:41:29 +0300

    Update utils.py

--- DeepPurpose/utils.py
+++ DeepPurpose/utils.py
@@ -531,11 +531,11 @@ def data_process(X_drug = None, X_target = None, y=None, drug_encoding=None, tar
 		print('in total: ' + str(len(df_data)) + ' drug-drug pairs')
 
 		if target_encoding == 'ProtTrans':
-			try:
-				from __main__ import df  # grabs df from notebook
-                                df_data['ProtTrans'] = df.loc[df_data.index, 'ProtTrans'].values
-			except Exception as e:
-				print("⚠️ Could not inject ProtTrans embeddings into df_data:", e)
+		    try:
+			from __main__ import df  # grabs df from notebook
+                        df_data['ProtTrans'] = df.loc[df_data.index, 'ProtTrans'].values
+		    except Exception as e:
+		        print("⚠️ Could not inject ProtTrans embeddings into df_data:", e)
 
 
 
